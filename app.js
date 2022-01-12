4135e639c6181631c5f63e0dbd9e74e507867d4f
LuoTheBuilder <rawbailey@outlook.com>
2022-01-12 12:01:07 -0500

    The list now works and elements can be added but there is no styling taking place

--- app.js
+++ app.js
@@ -1,59 +1,46 @@
+//SET UP THE STARTING CONSTANTS
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}));
+// var item="happy";
+var items =[];
+//CLOSE SET UP OF STARTING CONSTANTS
 
+            //SET UP INITIAL VARIABLES
+            var today = new Date();
+            var day = "";
+            var options ={
+              weekday: "long",
+              day: "numeric",
+              month: "long"
+            };
 
+            //FINISH SET UP OF INITIAL VARIABLES
+
+//GET PAGE FUNCTION
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var currentDate = today.getDate();
-  var currentMonth = today.getMonth() + 1;
-  var currentYear = today.getFullYear();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      theday = "sunday";
-      break;
-    case 1:
-      theday = "monday";
-      break;
-    case 2:
-      theday = "Tuesday"
-      break;
-    case 3:
-      theday = "Wednesday"
-      break;
-    case 4:
-      theday = "Thursday"
-      break;
-    case 5:
-      theday = "Friday"
-      break;
-    case 6:
-      theday = "Saturday"
-      break;
-    default:
-  }
-
-  if (currentDay === 6 || currentDay === 0) {
-    day = "weekend";
-  } else {
-    day = "weekday";
-  }
+var theday = today.toLocaleDateString("en-US", options);
   res.render('list', {
     kindOfDay: day,
     theday: theday,
-    todaydate: currentDate,
-    todaymonth: currentMonth,
-    todayyear: currentYear
+    newListItems: items
   })
-
 })
+//CLOSE OUT GET PAGE FUNCTION
 
-
-app.listen(3000, function(req, res) {
-  console.log('Alive!');
+//POST NEW ITEM FORM FUNCTION
+app.post("/", function(req, res){
+  item = req.body.actionItem;
+  items.push(item);
+  res.redirect("/");
 })
+//CLOSE POST NEW ITEM FORM FUNCTION
+
+          //SERVER LISTENER
+          app.listen(3000, function(req, res) {
+            console.log('Alive!');
+          })
+          //CLOSE OUT LISTENER
