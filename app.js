7c4f64b02843c5c8e5f9ece222a4b022eb0e5277
LuoTheBuilder <rawbailey@outlook.com>
2022-01-12 15:54:01 -0500

    Added a random adj api to create a unique subtitle every day.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //SET UP THE STARTING CONSTANTS
 const express = require("express");
+const https = require('https');
 const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
@@ -7,7 +8,17 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 // var item="happy";
 var items =[];
+var desc = "";
 //CLOSE SET UP OF STARTING CONSTANTS
+
+            //SET UP API FOR ADJECTIVES
+            let url = "https://random-word-form.herokuapp.com/random/adjective";
+            https.get(url, function(response){
+              response.on("data", function(data){
+                const adjective= JSON.parse(data);
+                desc = adjective;
+            })})
+            //END API SETUP
 
             //SET UP INITIAL VARIABLES
             var today = new Date();
@@ -17,13 +28,13 @@ var items =[];
               day: "numeric",
               month: "long"
             };
-
             //FINISH SET UP OF INITIAL VARIABLES
 
 //GET PAGE FUNCTION
 app.get("/", function(req, res) {
 var theday = today.toLocaleDateString("en-US", options);
   res.render('list', {
+    adj: desc,
     kindOfDay: day,
     theday: theday,
     newListItems: items
